0448fdba1b6576443f045a77d9d3d9e0bb43c871
jpark6 <qkstjr09@gmail.com>
2022-01-22 21:59:29 +0900

    Feat: Make TodoFooter Components

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,13 +1,40 @@
 <template>
-  <div></div>
+  <div class="clearAllContainer">
+    <button class="clearAllBtn" v-on:click="clearTodo">
+      <i class="fas fa-align-slash"></i>
+      Clear All
+    </button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-
+  methods: {
+    clearTodo(): void {
+      localStorage.clear();
+    },
+  },
 };
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.clearAllContainer {
+  display: flex;
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+  vertical-align: middle;
+  button {
+    background-color: transparent;
+    border: none;
+    margin: 0 auto;
+    &.clearAllBtn {
+      color: #e20303;
+      display: block;
+    }
+  }
+}
 </style>
